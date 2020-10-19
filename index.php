84b24f318203fff4496f68589dddf56ca4058f92
razikusman <razikusman@gmail.com>
2020-10-19 09:26:15 +0530

    foorter added

--- index.php
+++ index.php
@@ -10,17 +10,18 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </head>
     
-    <body style="background-color: silver;">
-        <div class="col-md-12 d-flex justify-content-around">
+    <body style="background-color: silver;font-family: Arial, Helvetica, sans-serif;">
+        <div class="col-md-12 d-flex justify-content-around" style ="margin: 0; padding: 0;">
             <?php
                 include("courosal.php");
             ?>
         </div>
-        <div id="navbar">
+        <div id="navbar" >
             <?php
                 include("sidebar.php");
             ?>
         </div>
+
         <div class="col-md-12  d-flex justify-content-around" >
             <div style="height: 100%; width: 75%">
                 <?php
@@ -28,33 +29,33 @@
                 ?>
             </div>
         </div>
-    </body>
-<!-- 
-    <script>
-        // When the user scrolls down 20px from the top of the document, slide down the navbar
-        window.onscroll = function() {scrollFunction()};
 
-        function scrollFunction() 
+        <div id="footer" >
+            <?php
+                include("footer.php");
+            ?>
+        </div>
+    </body>
+
+
+    
+    <!-- <script>
+        window.onscroll = function() {myFunction()};
+
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        function myFunction() 
         {
-            if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) 
+            if (window.pageYOffset >= sticky) 
             {
-                document.getElementById("navbar").style.top = "0";
+                navbar.classList.add("sticky")
             } 
-            else
+            else 
             {
-                document.getElementById("navbar").style.top = "400px";
+                navbar.classList.remove("sticky");
             }
         }
-    </script>
+    </script> -->
 
-    <style>
-
-        #navbar {
-        background-color: #333;
-        top: 600px;
-        width: 100%;
-        display: block;
-        transition: top 0.3s;
-        }
-    </style> -->
 </html>
