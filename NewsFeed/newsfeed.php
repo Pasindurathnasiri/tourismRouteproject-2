d3272da77dec6e4040774c17d94a60a273b3e8be
Pasindu Madhuranga <45681668+Pasindurathnasiri@users.noreply.github.com>
2020-10-28 18:40:28 +0530

    News Feed

---
+++ NewsFeed/newsfeed.php
@@ -0,0 +1,263 @@
+<style>
+   #cel{
+    color:black;
+    font-weight:bold;
+   
+
+  }
+
+  #cel:hover{
+    color:white;
+    font-weight:bold;
+    
+}
+
+ #nav{
+    color:white;
+    font-weight:bold;
+ }
+
+ #nav:hover{
+    color:white;
+    font-weight:bold;
+    
+ }
+</style>
+
+
+<nav class="navbar navbar-expand-lg" style="background: gray; width:100%;">
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="cel" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Celebrity 1
+        </a>
+      
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" id="cel" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Celebrity 1
+        </a>
+       
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" id="cel" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Celebrity 1
+        </a>
+        
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" id="cel" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Celebrity 1
+        </a>
+       
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" id="cel" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Celebrity 1
+        </a>
+      
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" id="cel" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Celebrity 1
+        </a>
+   
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+
+<nav class="navbar navbar-expand-lg" style="background: gray; width:100%;">
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="nav" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-weight:bold; color:black; background-color:Gold; border-radius:15px; padding:10px; width:300px;">
+          News Feed
+        </a>
+      
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" id="nav" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-weight:bold; color:black;     background-color:Gold; margin-left:10px; border-radius:15px; padding:10px; width:300px;">
+        User Profile
+        </a>
+       
+      </li>
+      
+   
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search News Feed" style="width:400px;" aria-label="Search">
+      <button class="btn btn-outline-dark my-2 my-sm-0" style="font-weight:bold; border-radius:15px;" type="submit">Search</button>
+    </form>
+   
+  </div>
+</nav>
+
+
+<div class="row">
+ <div class="col-md-3">
+ 
+<!-- row 1 start -->
+<div class="row" >
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+   <div class="carousel-inner" id ="add" >
+      <div class="carousel-item active  d-flex justify-content-end">
+        <img class="d-block"  src="images/gadd.png" alt="Second slide"> <!-- img -->
+      </div>
+      <div class="carousel-item  d-flex justify-content-end">
+        <img class="d-block"  src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="First slide"> <!-- img -->
+      </div>
+      <div class="carousel-item  d-flex justify-content-end" >
+        <img class="d-block"  src="images/sri-lanka-photos-0152.jpg.optimal.jpg" alt="Third slide"> <!-- img -->
+      </div>
+    </div>
+  </div> 
+</div>
+  <!-- row 1 end -->
+
+<!-- row 2 start -->
+<div class="row" >
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+   <div class="carousel-inner" id ="add" >
+      <div class="carousel-item active d-flex justify-content-end">
+        <img class="d-block"  src="images/gadd.png" alt="Second slide"> <!-- img -->
+      </div>
+      <div class="carousel-item d-flex justify-content-end">
+        <img class="d-block"  src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="First slide"> <!-- img -->
+      </div>
+      <div class="carousel-item d-flex justify-content-end" >
+        <img class="d-block"  src="images/sri-lanka-photos-0152.jpg.optimal.jpg" alt="Third slide"> <!-- img -->
+      </div>
+    </div>
+  </div> 
+</div>
+<!-- row 2 end -->
+
+<!-- row 3 start -->
+<div class="row" >
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+   <div class="carousel-inner" id = "add" >
+      <div class="carousel-item active  d-flex justify-content-end">
+          <img class="d-block"  src="images/gadd.png" alt="Second slide"> <!-- img -->
+      </div>
+      <div class="carousel-item  d-flex justify-content-end">
+          <img class="d-block"  src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="First slide"> <!-- img -->
+      </div>
+      <div class="carousel-item  d-flex justify-content-end" >
+          <img class="d-block"  src="images/sri-lanka-photos-0152.jpg.optimal.jpg" alt="Third slide"> <!-- img -->
+      </div>
+    </div>
+  </div>
+</div>
+<!-- row 3 end -->
+
+  <!-- row 4 start -->
+<div class="row" >
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+   <div class="carousel-inner" id = "add" >
+      <div class="carousel-item active d-flex justify-content-end">
+        <img class="d-block"  src="images/gadd.png" alt="Second slide"> <!-- img -->
+      </div>
+      <div class="carousel-item d-flex justify-content-end">
+        <img class="d-block" src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="First slide"> <!-- img -->
+      </div>
+      <div class="carousel-item d-flex justify-content-end" >
+        <img class="d-block" src="images/sri-lanka-photos-0152.jpg.optimal.jpg" alt="Third slide"> <!-- img -->
+      </div>
+    </div>
+  </div> 
+</div>
+ </div>
+ <div class="col-md-9">
+ <div class="card" style="width: 80%; padding:10px;">
+  <img class="card-img-top" src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Post 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-warning">Like</a>
+    <a href="#" class="btn btn-warning">Comment</a>
+    
+  </div>
+  
+</div>
+
+<!--  -->
+
+<div class="card" style="width: 80%; padding:5px; margin-top:10px;">
+  <img class="card-img-top" src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Post 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-warning">Like</a>
+    <a href="#" class="btn btn-warning">Comment</a>
+ 
+  </div>
+  
+</div>
+
+
+<div class="card" style="width: 80%; padding:5px; margin-top:10px;">
+  <img class="card-img-top" src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Post 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-warning">Like</a>
+    <a href="#" class="btn btn-warning">Comment</a>
+ 
+  </div>
+  
+</div>
+
+
+<div class="card" style="width: 80%; padding:5px; margin-top:10px;">
+  <img class="card-img-top" src="images/23-232752_beautiful-sri-lanka-archive-sri-lanka-tourism-hd.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Post 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-warning">Like</a>
+    <a href="#" class="btn btn-warning">Comment</a>
+ 
+  </div>
+  
+</div>
+
+ </div>
+</div>
+
+<!-- row 4 end -->
+
+<!-- styles start -->
+<style>
+  img{
+    
+    height: 600px;
+    width:250px;
+    padding:5px;
+  }
+
+  #add{
+    height: 600px;
+    width:250px;
+   
+    border: 3px solid gold;
+  }
+  .row{
+    margin-left:20px;
+    padding:5px;
+  }
+</style>
+
+
---
+++ newsfeedMain.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tourism Web Site - News Feed</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </head>
+    
+    <style>
+    body{
+        background-image:url("images/background test2(1).jpg");
+        background-size:200%;
+    }
+    </style>
+    <!-- body begin -->
+    <body style="font-family: Arial, Helvetica, sans-serif;">
+
+     
+
+        <!-- top bar start -->
+        <div id="navbar" >
+            <?php
+                include("sidebar.php");
+            ?>
+        </div>
+        <!-- top bar end -->
+
+        <!-- content start  -->
+        <?php
+                include("NewsFeed/newsfeed.php");
+       ?>
+
+        <!-- content end -->
+        <!-- footer start -->
+        <div id="footer" >
+            <?php
+                include("footer.php");
+            ?>
+        </div>
+        <!-- footer end -->
+    </body>
+
+
+
+</html>
